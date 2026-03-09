05c02bf9c4bb383daf7d61674a0dc8bd677f7f01
lequangdung <lequangdung2906@gmail.com>
2026-03-09 21:36:59 +0700

    them giao dien

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,12 @@ import SignInPages from './pages/auth/SignInPages'
 import Home from './pages/home/Home'
 import QuestionDetail from './pages/question/QuestionDetail'
 import CreateQuestion from './pages/question/CreateQuestion'
+import Questions from './pages/questions/Questions'
+import Profile from './pages/profile/Profile'
+import SettingProfile from './pages/profile/SettingProfile'
+import Security from './pages/securities/Security'
+import EmailChange from './pages/securities/emailchange'
+import EmailAuth from './pages/securities/EmailAuth'
 import { Toaster } from 'sonner'
 import { useAuthStore } from './stores/useAuthStore'
 
@@ -47,12 +53,36 @@ function App() {
             element={<Home />}
           />
           <Route
+            path='/questions'
+            element={<Questions />}
+          />
+          <Route
             path='/questions/:id'
             element={<QuestionDetail />}
           />
           <Route
             path='/ask'
             element={<CreateQuestion />}
+          />
+          <Route
+            path='/profile'
+            element={<Profile />}
+          />
+          <Route
+            path='/settings/profile'
+            element={<SettingProfile />}
+          />
+          <Route
+            path='/security'
+            element={<Security />}
+          />
+          <Route
+            path='/security/email-change'
+            element={<EmailChange />}
+          />
+          <Route
+            path='/security/email-auth'
+            element={<EmailAuth />}
           />
           {/* Private Route example - Keep ProtectedRoute for future private pages */}
           {/* <Route element={<ProtectedRoute />}>
