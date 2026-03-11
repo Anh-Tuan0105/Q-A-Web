47d938e7f5175968e38a6294de646343f909a269
Anh-Tuan0105 <huynhngocanhtuan9a9@gmail.com>
2026-03-11 17:38:18 +0700

    deploy vercel 1

--- backend/src/helpers/mailSend.js
+++ backend/src/helpers/mailSend.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer'
 export const sendMail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
+        port: 465, //587
+        secure: true, //false
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
