bab294cab8b9540c0174e76878b52037e5e75b6d
Anh-Tuan0105 <huynhngocanhtuan9a9@gmail.com>
2026-03-11 22:15:28 +0700

    gmail24

--- backend/src/helpers/mailSend.js
+++ backend/src/helpers/mailSend.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const sendMail = async (to, subject, html) => {
     await axios.post(
-        "https://script.google.com/macros/s/AKfycbwoAnUvgfWSyP3KAhzZLgXQ-4tQskFJFX-SntpUqmT6ZSdzRuMVMuULLiFsYd1K1Pu-/exec",
+        process.env.GOOGLE_SCRIPT_URL,
         {
             to,
             subject,
