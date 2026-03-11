8671b159fe89acacb73f3599ba2bc6147c02aa54
Anh-Tuan0105 <huynhngocanhtuan9a9@gmail.com>
2026-03-11 21:53:00 +0700

    gmail22

--- backend/src/helpers/mailSend.js
+++ backend/src/helpers/mailSend.js
@@ -1,39 +1,12 @@
-import nodemailer from 'nodemailer';
+import axios from "axios";
 
-export const sendMail = async (to, subject, text) => {
-    if (!process.env.BREVO_USER || !process.env.BREVO_SMTP_KEY) {
-        console.error("Lỗi: Thiếu biến môi trường EMAIL_USER hoặc EMAIL_PASS");
-        throw new Error("Cấu hình Email không tồn tại trên Server");
-    }
-
-    console.log(`Đang khởi tạo Nodemailer để gửi tới: ${to}...`);
-
-    // Create a transporter object
-    const transporter = nodemailer.createTransport({
-        host: "smtp-relay.brevo.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.BREVO_USER,
-            pass: process.env.BREVO_SMTP_KEY,
-        },
-    });
-
-    // Configure the mailoptions object
-    const mailOptions = {
-        from: '"QA App" <huynhngocanhtuan9a7@gmail.com>',
-        to: to,
-        subject: subject,
-        html: text,
-    };
-
-    // Send the email
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log("Error:", error);
-        } else {
-            console.log("Email sent: ", info.response);
+export const sendMail = async (to, subject, html) => {
+    await axios.post(
+        "https://script.google.com/macros/s/AKfycbwoAnUvgfWSyP3KAhzZLgXQ-4tQskFJFX-SntpUqmT6ZSdzRuMVMuULLiFsYd1K1Pu-/exec",
+        {
+            to,
+            subject,
+            html,
         }
-    });
-
-}
+    );
+};
